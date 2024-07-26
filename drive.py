c8a2ca232604e0cd821646254e938322e46750db
Atharv Patwardhan <84259526+atharvpatwardhan@users.noreply.github.com>
2024-07-26 11:49:06 -0400

    Update drive.py

--- drive.py
+++ drive.py
@@ -52,6 +52,6 @@ def send_control(steering_angle, throttle):
  
  
 if __name__ == '__main__':
-    model = load_model('model1.h5')
+    model = load_model('model.h5')
     app = socketio.Middleware(sio, app)
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
